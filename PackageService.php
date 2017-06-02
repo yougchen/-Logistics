bbe73a60332e79128073b36468152a48237c39bd
N551JB\陳永平 <milk19961014@gmail.com>
2017-06-02 20:21:18 +0800

    no message

--- PackageService.php
+++ PackageService.php
@@ -1,3 +1,26 @@
+<title>
+	急速快遞
+</title>
+<body>
+
+			<font size="100">急速快遞</font>
+
+			<br/>
+
+            <a href="Service.php">商品服務</a>
+
+            <a href="Send.php">寄件</a>
+   
+            <a href="Recive.php">收件</a>
+            
+  
+            <a href="Search.php">查詢</a>
+  
+            <a href="Account.php">帳號</a>
+            <br/>
+            
+ </body>
+</html>		
 <?php
 if ($_GET["factor"]=="常溫宅急便")
 {
