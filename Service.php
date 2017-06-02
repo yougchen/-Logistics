3ea3627d3bf5eeeeba8f8e745880073d31bbbc7d
N551JB\陳永平 <milk19961014@gmail.com>
2017-06-02 20:11:53 +0800

    update

--- Service.php
+++ Service.php
@@ -1,10 +1,28 @@
 <!DOCTYPEhtml>
 <html>
 <head>
-	<title>商品服務</title>
-</head>
+<title>
+	急速快遞
+</title>
 <body>
-			
+
+			<font size="100">急速快遞</font>
+
+			<br/>
+
+            <a href="Service.php">商品服務</a>
+
+            <a href="Send.php">寄件</a>
+   
+            <a href="Recive.php">收件</a>
+            
+  
+            <a href="Search.php">查詢</a>
+  
+            <a href="Account.php">帳號</a>
+            
+ </body>
+</html>		
 
 <?php
 echo "<form action=\"PackageService.php\" method=\"post\" >";
