95128eb4c9300f7e1d766159c28eddfd78bfd8e6
LamHei cheng <zak19951205@yahoo.com.hk>
2017-06-07 09:50:09 +0800

    css

--- serch_pac_type_m.php
+++ serch_pac_type_m.php
@@ -96,6 +96,18 @@ table {
       font-family: 微軟正黑體;
       margin: 10px 10px 10px 10px;
       padding: 8px 20px 8px 20px;
+      line-height: 300%;
+    }
+
+    table, th, td {
+    border: 1px solid black;
+    }  
+
+    table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 0px 0px 0px 10px;
+
     }
 </style>
 
