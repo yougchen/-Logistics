2ced64cf729590731ce5a94eb53f51c314e89dd5
N551JB\陳永平 <milk19961014@gmail.com>
2017-06-22 19:33:06 +0800

    no message

--- actionlogin.php
+++ actionlogin.php
@@ -24,6 +24,7 @@
 
 <h4>
 <?php
+ob_start();
 session_start();
 include("config.php");
 
@@ -38,19 +39,18 @@ $row = mysqli_fetch_assoc($result);
 if ($total_records > 0) {
     if($row["manager_right"] == 0) {
        $_SESSION["loginsession"] = $account;
-       ob_start();
        session_write_close();
        header("Location:function.php");die();
         ob_end_flush(); 
     } else {
         $_SESSION["loginsession"] = $account;
-        ob_start();
         session_write_close();
         header("Location:manager.php");die();
          ob_end_flush(); 
     }
 } else {
-    header("refresh:3;url=login.php");    
+    header("refresh:3;url=login.php");
+    ob_end_flush();    
     echo "登入失敗!!!";
 }
 ?>
